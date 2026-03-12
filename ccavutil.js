f515b170de9ca3060fb2ee01489b39f1b5d059fb
animagdum497 <magdumaniket497@gmail.com>
2026-03-12 18:44:32 +0530

    encryption logic changed

--- ccavutil.js
+++ ccavutil.js
@@ -1,14 +1,12 @@
 const crypto = require("crypto");
 
-exports.encrypt = function (plainText, workingKey) {
-  const m = crypto.createHash("md5");
-  m.update(workingKey);
-
-  const key = m.digest();
+function encrypt(plainText, workingKey) {
   const iv = Buffer.from([
     0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b,
     0x0c, 0x0d, 0x0e, 0x0f,
   ]);
+
+  const key = Buffer.from(workingKey.substr(0, 16));
 
   const cipher = crypto.createCipheriv("aes-128-cbc", key, iv);
 
@@ -16,4 +14,6 @@ exports.encrypt = function (plainText, workingKey) {
   enc += cipher.final("hex");
 
   return enc;
-};
+}
+
+module.exports = { encrypt };
